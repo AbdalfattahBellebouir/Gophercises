0bb20a0131ea0c7ca1b4c437fa1bdc98eb1999ca
Abdalfattah BELLEBOUIR <73496796+AbdalfattahBellebouir@users.noreply.github.com>
2022-12-28 16:38:27 +0100

    + updates

--- Exercise2 URL shortner/handler.go
+++ Exercise2 URL shortner/handler.go
@@ -12,7 +12,15 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -33,5 +41,6 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
+
 	return nil, nil
 }
--- Exercise2(URL shortner)/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/AbdalfattahBellebouir/gophercises
-
-go 1.17
-
-require github.com/gophercises/urlshort v0.0.0-20190723121003-cc800dbaf411
